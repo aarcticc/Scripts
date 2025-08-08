5acbff183c7e16fbf21c904ff8d7fe775471b353
aarcticc <hcf2509@proton.me>
2025-08-08 17:35:18 +0200

    yea

--- scripts/C/leaning/main.c
+++ scripts/C/leaning/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-// #include "main.h"
 
 int main(){
+    char* anrede[256];
+    int anhaenge = 2;
 
+    scanf("%s[256]", &anrede);
+    	 
+    printf("%s\n\n", anrede);
+    printf("Hier sind die Unterlagen:\n\n");
 
+    printf("Anhänge: %d", anhaenge);
+    
     return 0;
 }
