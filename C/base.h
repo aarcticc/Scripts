a788adce2ceb71331f3744ef6abe6043bb9e99dc
aarcticc <hcf2509@proton.me>
2025-09-17 13:33:53 +0200

    idk anymore what i did there

--- C/base.h
+++ C/base.h
@@ -1,1 +1,1 @@
-// if I ever make a Codebase it comes here
+// if I ever make a Codebase it goes here
---
+++ C/testing/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#incldue <string.h>
+
+int main(int ){
+    
+    
+    return 0;
+}
