462b60278ba5d568ae30887b1b1895779869da1f
aarcticc <hcf2509@proton.me>
2025-10-07 22:15:03 +0200

    refactor: reorganize main menu and bookshelf functions

--- C/something/include/bookshelf.h
+++ C/something/include/bookshelf.h
@@ -3,44 +3,6 @@
 
 #ifdef
 #define BOOL YESNO
-
-
-char bookshelf[100][256];
-
-void listAllBooks(){
-
-}
-
-void displayBookCount(){
-
-}
-
-void addBook(){
-	char bookName[256];
-	char bookAuthor[100];
-	char tempDecision;
-
-	printf("What book should be addet?\n\n");
-
-	printf("Name: \n");
-	scanf("%s", &bookName);
-
-	printf("Author: \n");
-	scanf("%s", &bookAuthor);
-
-	printf("Do you want to add: %s, %s. [TRUE/false]? ", bookName, bookAuthor);
-	while (scanf("%c", &tempDecision) < 3){
-		if (tempDecision == 2)
-			displayBookCount();
-		}
-	} else {
-		printf("This is not a valid input!");
-		addBook();
-	}
-}
-
-void exitProgram(){
-
-}
+char bookshelf [100][256];
 
 #endif
--- C/something/src/main.c
+++ C/something/src/main.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include "bookshelf.h"
 
-int main(){
+int main() {
 	start(mainMenu());
 
 	return 0;
 }
 
+void start(int nextIndex /*to be declared*/, char ) {
+	switch(mainMenu) {
+		case 0: 
+			break;
+
+		case 1:
+			break;
+
+		case 2:
+			break;
+
+		case 3:
+			break;
+
+		default:
+			printf(ERROR) // maybe change		
+	}
+
+}
+
 void mainMenu() {
-	int menuSelection;
+	int selection;
 
 	printf("------------------------------\n");
 	printf("------(0)-LIST-ALL-BOOKS------\n"); // sort authors in submenu
@@ -18,26 +38,12 @@ void mainMenu() {
 	printf("------------------------------\n\n");
 
 	printf("What do you want to do?: \n");
-	scanf("%d", &menuSelection);
-
-	switch(menuSelection) {
-		case 0:
-			listAllBooks();
-			break;
-		case 1:
-			displayBookCount();
-			break;
-		case 2:
-			addBook();
-			break;
-		case 3:
-			exitProgram();
-			break;
-
-		default:
-			printf("That is not an option\n");
-			break;
+	scanf("%d", &selection);
+	printf("\n\n");
+	if(selection == 0 || selection == 1 || selection == 2 || selection == 3 || selection == 4) {
+		return selection;
+	} else {
+		printf("This is not a valid option!\n");
+		return mainMenu();
 	}
 }
-
-
